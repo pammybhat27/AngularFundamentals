522e5d43fc58e33697aa44f5f8ebb0c4392af5a9
Param <pammybhat27@gmail.com>
2018-07-21 18:42:37 +1000

    Imports contain required dependencies
    Declarations contain  dfifferent components that are in use in the particular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,1 +1,21 @@
+import { NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import { CommonModule} from '@angular/common';
 
+@NgModule({
+  bootstrap: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule
+  ],
+  declarations: [AppComponent]
+
+
+})
+
+
+export class AppModule {}
