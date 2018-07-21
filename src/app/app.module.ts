b3679eeb5d5a68ade4fc7916c920a2fce97b9231
Param <pammybhat27@gmail.com>
2018-07-21 20:23:06 +1000

    Import FormsModule for 2 way data binding
    Importing the forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import { CommonModule} from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   bootstrap: [
@@ -10,7 +11,8 @@ import { CommonModule} from '@angular/common';
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [AppComponent]
 
