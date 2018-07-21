40c2ade98605f3608eaf523916bcb930fa661cfd
Param <pammybhat27@gmail.com>
2018-07-21 18:31:05 +1000

    Adding Component and registering it
    Adding a property called title, and displaying it using string interpolation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,1 +1,25 @@
+import { Component } from '@angular/core';
 
+
+
+@Component(
+  {
+    selector: 'app-root',
+    template: `
+    <div>
+      {{ title }}
+    </div>
+
+    `
+  }
+)
+export class AppComponent {
+  title: string;
+
+  constructor() {
+    this.title = 'Ultimate Angular';
+
+  }
+
+
+}
