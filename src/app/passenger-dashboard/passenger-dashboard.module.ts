f22f2a61c94938cc2e188017a046e1fbd283cdfe
Param <pammybhat27@gmail.com>
2018-07-23 19:17:17 +1000

    Creating the passengerdashboard module

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,6 +1,16 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule
+  ]
+
+})
 export class PassengerDashboardModule {
 
 
