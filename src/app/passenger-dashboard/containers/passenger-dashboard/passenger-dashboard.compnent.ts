e51bff01c8c5f91a36d59493414e9ba80bdc61b2
Param <pammybhat27@gmail.com>
2018-07-23 23:33:09 +1000

    Edit event emitter

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.compnent.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.compnent.ts
@@ -63,8 +63,16 @@ export class PassengerDashboardComponent implements OnInit {
   }
 
   handleEdit(event) {
-    console.log(event);
+  this.passengers = this.passengers.map((passenger: Passenger) => {
 
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+    return passenger;
+    }
+
+  );
+console.log(this.passengers);
   }
 
 
