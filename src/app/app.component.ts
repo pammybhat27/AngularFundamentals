d85b835a18c77b7c83015056fa5fc0728283762a
Param <pammybhat27@gmail.com>
2018-07-23 19:32:53 +1000

    First step to braking up app.component.ts
    Move relevanat data from app.component.ts to passenger-dashboard.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,43 +20,7 @@ id: number;
     template: `
     <div class = 'app'>
       <h3> Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span class = "status"
-          [class.checked-in]="passenger.checkedIn">
 
-          </span>
-
-          {{ i }} : {{passenger.fullName}}
-        </li>
-
-      </ul>
-
-      <h3> Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span class = "status"
-              [ngStyle] ="{ backgroundColor: (passenger.checkedIn?'#2ecc71':'#c0392b')}"
-
-
-
-          >
-
-          </span>
-
-          {{ i }} : {{passenger.fullName}}
-      
-          <div class = "date">
-          Check in Date:
-            {{passenger.checkInDate? (passenger.checkInDate | date | uppercase : 'yMMMMd') :'Not checked in'}}
-          </div>
-          
-          <div class = "children">
-            children: {{passenger.children?.length || 0}}
-          </div>
-        </li>
-
-      </ul>
 
 
 
